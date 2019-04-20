ad37ba91abdf64a8660d5c606daef3dad4529e68
Andy Li <ali37@uic.edu>
2019-04-19 22:00:27 -0500

    Added index DB, service worker, manifest templates. Content Update

---
+++ sw.js
@@ -0,0 +1,28 @@
+version = '1.0';
+
+let cacheName = 'AL' + version;
+
+self.addEventListener('install', e => {
+  let timeStamp = Date.now();
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll([
+        `./`,
+        `./index.html`
+      ])
+      .then(() => self.skipWaiting());
+    })
+  )
+});
+
+self.addEventListener('activate',  event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request, {ignoreSearch:false}).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
